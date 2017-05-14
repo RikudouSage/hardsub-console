88f216ad4b203f9c99553b490f1ef4c1ce8fd9f7
Dominik Chrástecký <dominik@chrastecky.cz>
2017-05-14 04:15:27 +0200

    czech localization updated

--- source/hardsub/localizations/cs_CZ.ts
+++ source/hardsub/localizations/cs_CZ.ts
@@ -4,173 +4,266 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="../main.qml" line="29"/>
+        <location filename="../main.qml" line="38"/>
         <source>Hardsub Console</source>
         <translation>Hardsub Konzole</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="49"/>
+        <location filename="../main.qml" line="66"/>
         <source>Choose the original video file</source>
         <translation>Zvolte soubor s původním videem</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="60"/>
-        <location filename="../main.qml" line="229"/>
+        <location filename="../main.qml" line="77"/>
+        <location filename="../main.qml" line="236"/>
+        <location filename="../main.qml" line="418"/>
+        <location filename="../main.qml" line="537"/>
         <source>Select video</source>
         <translation>Zvolit video</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="68"/>
+        <location filename="../main.qml" line="85"/>
         <source>Choose the subtitles file</source>
         <translation>Zvolte soubor s titulky</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="79"/>
-        <location filename="../main.qml" line="251"/>
+        <location filename="../main.qml" line="96"/>
+        <location filename="../main.qml" line="559"/>
         <source>Select subtitles</source>
         <translation>Zvolit titulky</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="87"/>
+        <location filename="../main.qml" line="104"/>
         <source>Choose the folder the hardsub will be saved to</source>
         <translation>Zvolte složku, do které bude hardsub uložen</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="98"/>
+        <location filename="../main.qml" line="115"/>
         <source>Select folder</source>
         <translation>Zvolit složku</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="107"/>
+        <location filename="../main.qml" line="124"/>
         <source>Choose the output filename</source>
         <translation>Zvolte název souboru s hardsubem</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="121"/>
+        <location filename="../main.qml" line="138"/>
         <source>Input the output video bitrate in kB</source>
         <translation>Zadejte bitrate hardsubu v kB</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="132"/>
+        <location filename="../main.qml" line="149"/>
         <source>Create hardsub</source>
         <translation>Vytvořit hardsub</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="150"/>
-        <location filename="../main.qml" line="363"/>
+        <location filename="../main.qml" line="167"/>
+        <location filename="../main.qml" line="373"/>
+        <location filename="../main.qml" line="671"/>
         <source>Error</source>
         <translation>Chyba</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="151"/>
+        <location filename="../main.qml" line="168"/>
         <source>You need to fill all the fields.</source>
         <translation>Musíte vyplnit všechna pole.</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="152"/>
-        <location filename="../main.qml" line="365"/>
+        <location filename="../main.qml" line="169"/>
+        <location filename="../main.qml" line="673"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="174"/>
+        <location filename="../main.qml" line="204"/>
+        <source>Extract MKV file</source>
+        <translation>Extrahování MKV souboru</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="222"/>
+        <source>Choose mkv file</source>
+        <translation>Zvolte mkv soubor</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="251"/>
+        <source>Loading info...</source>
+        <translation>Načítání informací...</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="275"/>
+        <source>The count of video streams: %1</source>
+        <translation>Počet video streamů: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="280"/>
+        <source>The count of audio streams: %1</source>
+        <translation>Počet audio streamů: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="285"/>
+        <source>The count of subtitle files: %1</source>
+        <translation>Počet souborů s titulky: %1</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="297"/>
+        <source>Extract subtitles</source>
+        <translation>Extrahovat titulky</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="304"/>
+        <source>Create copy of video without subtitles</source>
+        <translation>Vytvořit kopii videa bez titulků</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="325"/>
+        <source>Select folder to save subtitles</source>
+        <translation>Zvolte složku pro uložení titulků</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="358"/>
+        <source>Subtitles were successfully extracted. Do you want to open the folder now?</source>
+        <translation>Titulky byly úspěšně extrahovány. Chcete nyní otevřít složku s titulky?</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="374"/>
+        <source>Could not extract subtitles. Make sure the target directory exists and does not contain a file with same name.</source>
+        <translation>Nepodařilo se extrahovat titulky. Ujistěte se, že složka existuje a neobsahuje soubor se stejným jménem.</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="409"/>
+        <source>Failed to read data from file!</source>
+        <translation>Nepodařilo se ze souboru načíst data!</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="439"/>
+        <source>Coult not find the MKVToolnix. Please install it according to your distribution.</source>
+        <translation>Nepodařilo se najít MKVToolnix . Prosím, nainstalujte jej pro svou distribuci.</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="449"/>
+        <source>Install MKVToolnix</source>
+        <translation>Nainstalovat MKVToolnix</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="461"/>
+        <source>Check MKVToolnix again</source>
+        <translation>Zkontrolovat dostupnost MKVToolnixu</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="478"/>
         <source>Stop hardsub creation</source>
         <translation>Zastavit vytváření hardsubu</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="185"/>
+        <location filename="../main.qml" line="489"/>
         <source>Hardsub is being created (%1%)</source>
         <translation>Probíhá vytváření hardsubu (%1 %)</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="203"/>
-        <location filename="../main.qml" line="325"/>
-        <location filename="../main.qml" line="375"/>
+        <location filename="../main.qml" line="507"/>
+        <location filename="../main.qml" line="633"/>
+        <location filename="../main.qml" line="683"/>
         <source>0 min.</source>
         <translation>0 min.</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="210"/>
+        <location filename="../main.qml" line="514"/>
         <source>Remaining: %1</source>
         <translation>Zbývá přibližně: %1</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="231"/>
+        <location filename="../main.qml" line="420"/>
+        <location filename="../main.qml" line="539"/>
         <source>Videos %1</source>
         <translation>Videa %1</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="253"/>
+        <location filename="../main.qml" line="528"/>
+        <source>Hardsub</source>
+        <translation>Hardsub</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="531"/>
+        <source>MKV edit</source>
+        <translation>Úprava MKV</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="561"/>
         <source>Subtitles %1</source>
         <translation>Titulky %1</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="253"/>
+        <location filename="../main.qml" line="561"/>
         <source>All files %1</source>
         <translation>Všechny soubory %1</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="262"/>
+        <location filename="../main.qml" line="570"/>
         <source>Choose the output dir</source>
         <translation>Zvolte složku pro uložení hardsubu</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="305"/>
+        <location filename="../main.qml" line="613"/>
         <source>New version available!</source>
         <translation>Nová verze k dispozici!</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="306"/>
+        <location filename="../main.qml" line="614"/>
         <source>New version (%1) of this app is available! Do you want to download it now?</source>
         <translation>Nová verze (%1) této aplikace je k dispozici! Chcete ji nyní stáhnout?</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="307"/>
+        <location filename="../main.qml" line="615"/>
         <source>Never</source>
         <translation>Nikdy</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="310"/>
+        <location filename="../main.qml" line="618"/>
         <source>No</source>
         <translation>Ne</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="313"/>
+        <location filename="../main.qml" line="621"/>
         <source>Yes</source>
         <translation>Ano</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="342"/>
+        <location filename="../main.qml" line="357"/>
+        <location filename="../main.qml" line="650"/>
         <source>Done</source>
         <translation>Hotovo</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="343"/>
+        <location filename="../main.qml" line="651"/>
         <source>Hardsub created succesfully! Do you want to open the output folder now?</source>
         <translation>Hardsub byl úspěšně vytvořen! Chcete nyní otevřít složku s hardsubem?</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="344"/>
+        <location filename="../main.qml" line="359"/>
+        <location filename="../main.qml" line="375"/>
+        <location filename="../main.qml" line="652"/>
         <source>Close</source>
         <translation>Zavřít</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="347"/>
+        <location filename="../main.qml" line="362"/>
+        <location filename="../main.qml" line="655"/>
         <source>Open folder</source>
         <translation>Otevřít složku</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="364"/>
+        <location filename="../main.qml" line="672"/>
         <source>One of the selected files does not exist</source>
         <translation>Jeden ze zadaných souborů neexistuje</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="403"/>
+        <location filename="../main.qml" line="706"/>
         <source>min.</source>
         <translation>min.</translation>
     </message>
     <message>
-        <location filename="../main.qml" line="406"/>
+        <location filename="../main.qml" line="709"/>
         <source>h</source>
         <translation>h</translation>
         <extra-Context>Short for hour</extra-Context>
